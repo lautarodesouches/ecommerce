b3dcaef441979a9cbad5521f9bf30a7bca3b6c15
Lautaro Desouches <lautarodesouches@outlook.com>
2022-03-21 15:11:55 -0300

    move folder

---
+++ modules/urls.mjs
@@ -0,0 +1,19 @@
+export const urlInicio          = '/';
+export const urlIniciarSesion   = '#';
+export const urlRegistrarse     = '#';
+export const urlCategorias      = '/categorias/';
+export const urlOfertas         = '/ofertas/';
+export const urlFavoritos       = '/favoritos/';
+export const urlCarrito         = '/cart/';
+export const urlProducto        = '/producto/';
+export const urlBuscador        = '/buscador/';
+
+export const urlAyuda           = '/help/';
+export const urlContacto        = '/contact/';
+export const urlPrivacidad      = '/privacy/';
+
+export const urlCarpetaImg      = '/img/';
+
+export const urlProductosJSON   = '/json/productos.json';
+
+export const urlPagar           = '/checkout/';
