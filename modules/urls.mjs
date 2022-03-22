81f7ba231856a40887ade289b12e500ddd0ed4e3
Lautaro Desouches <lautarodesouches@outlook.com>
2022-03-22 12:23:27 -0300

    change urls to GitHub pages

--- modules/urls.mjs
+++ modules/urls.mjs
@@ -1,17 +1,17 @@
-export const urlInicio          = '/';
-export const urlCategorias      = '/categorias/';
-export const urlOfertas         = '/ofertas/';
-export const urlFavoritos       = '/favoritos/';
-export const urlCarrito         = '/cart/';
-export const urlProducto        = '/producto/';
-export const urlBuscador        = '/buscador/';
+export const urlInicio          = 'https://lautarodesouches.github.io/ecommerce/';
+export const urlCategorias      = 'https://lautarodesouches.github.io/ecommerce/categorias/';
+export const urlOfertas         = 'https://lautarodesouches.github.io/ecommerce/ofertas/';
+export const urlFavoritos       = 'https://lautarodesouches.github.io/ecommerce/favoritos/';
+export const urlCarrito         = 'https://lautarodesouches.github.io/ecommerce/cart/';
+export const urlProducto        = 'https://lautarodesouches.github.io/ecommerce/producto/';
+export const urlBuscador        = 'https://lautarodesouches.github.io/ecommerce/buscador/';
 
-export const urlAyuda           = '/help/';
-export const urlContacto        = '/contact/';
-export const urlPrivacidad      = '/privacy/';
+export const urlAyuda           = 'https://lautarodesouches.github.io/ecommerce/help/';
+export const urlContacto        = 'https://lautarodesouches.github.io/ecommerce/contact/';
+export const urlPrivacidad      = 'https://lautarodesouches.github.io/ecommerce/privacy/';
 
-export const urlCarpetaImg      = '/img/';
+export const urlCarpetaImg      = 'https://lautarodesouches.github.io/ecommerce/img/';
 
-export const urlProductosJSON   = '/json/productos.json';
+export const urlProductosJSON   = 'https://lautarodesouches.github.io/ecommerce/json/productos.json';
 
 export const urlPagar           = '/checkout/';
